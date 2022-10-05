dbf7c3675a57abac3f34ed5cbd65d12200651071
Igor <99yearago99@gmail.com>
2022-10-05 23:53:25 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-    int salary = 15_000;
     double savings = 15_000f;
     int month = 1;
 
     while (savings<12_000_000){
         month++;
         savings = savings + (savings*0.07);
-        savings = savings + salary;
         System.out.printf("Месяц %d, сумма накоплений составляет %f\n", month, savings);
 
     }
