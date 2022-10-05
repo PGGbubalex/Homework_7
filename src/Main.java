c4949f89f147bfb09f2a04297741bc83f013c986
Igor <99yearago99@gmail.com>
2022-10-06 02:37:04 +0300

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        int cometInterval = 79;
+        int twoHundredYearsAgo = 1822;
+        int hundredYearsAfter = 2122;
+        int year = 0;
 
+        do {
+            year++;
+            if (year%cometInterval==0 &&year>twoHundredYearsAgo)
+                System.out.println(year);
+        }
+        while (year <= hundredYearsAfter);
     }
 }
