9fa15cde7d90b3ec0623b565f36a088eee8f5647
Igor <99yearago99@gmail.com>
2022-10-05 21:58:36 +0300

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        int salary = 15_000;
+        double savings = 15_000f;
+        int month = 1;
 
+        while (savings < 12_000_000) {
+            month++;
+            savings = savings + (savings * 0.07);
+            savings = savings + salary;
+            if (month % 6 == 0) {
+                System.out.println("За " + month + " месяц Василий накопил " + savings + " рублей.");
+            }
+        }
     }
 }
