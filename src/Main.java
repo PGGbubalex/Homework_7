f46c14f3622407bde20acf26b34350a39a0f91fd
Igor <99yearago99@gmail.com>
2022-10-05 19:59:15 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        int deposit = 15_000;
+        int savingsTotal = 0;
+        int month = 0;
 
+        while (savingsTotal<=2_459_000){
+            month++;
+            savingsTotal = savingsTotal+savingsTotal/100;
+            savingsTotal = savingsTotal + deposit;
+            System.out.println("Месяц " + month + " ,сумма накоплений равна " + savingsTotal + " рублей");
+        }
     }
 }
