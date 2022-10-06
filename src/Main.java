41e436e76f720915c79a9b407a5dd5ef6c9ddcfe
Igor <99yearago99@gmail.com>
2022-10-06 03:43:20 +0300

    Task 9

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+    int b;
+    int c = 2;
 
+    for (int a = 1;a<=10; a++ ){
+    b = a*c;
+        System.out.println("2*" + a + "=" + b);
+    }
     }
 }
